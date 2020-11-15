cea1ff66e33d9e8e2fa4ec82290aeeaaf9b8f67a
Parteek <sparteek9@gmail.com>
2020-11-15 12:25:21 +0530

    third commit

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from sys import version
 from setuptools import _install_setup_requires, setup
 
-setup(name='topsis',version='0.1',packages=['topsis'],install_requires=['pandas','numpy'])
+ls=open("desc.txt","r")
+setup(name='TOPSIS-parteekpal-101803190',version='0.3',packages=['topsis'],install_requires=['pandas','numpy'],long_description=ls.read())
